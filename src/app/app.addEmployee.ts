1e5259e285f42f8abeb7c366bbadd41336da4ad2
Hardika25 <51956314+Hardika25@users.noreply.github.com>
2019-07-08 15:08:38 +0530

    Add files via upload
    Demo one

---
+++ src/app/app.addEmployee.ts
@@ -0,0 +1,11 @@
+
+import {Component} from '@angular/core';
+@Component({
+   selector:'add-emp',
+   template:`<h1> in Add Employee Component...</h1>`
+
+})
+export class AddEmployeeComponent
+{
+
+}
